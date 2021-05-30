f2cf60850e9aa240a5c4682ac9c6a2bf0a0131f0
saricay <33890430+saricay@users.noreply.github.com>
2021-05-30 23:40:35 +0300

    Add files via upload

--- src/inverse_kinematics.py
+++ src/inverse_kinematics.py
@@ -1,15 +1,30 @@
 #! /usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
+def limit_duty(duty):
+	print duty
+	if abs(duty)>100:
+		print("Duty is regulated")
+		duty=duty/abs(duty)*100
+	else:
+		print("Duty is OK")
+		duty=duty
+	return duty
 
 def callback(msg):
 	global right_motor_duty, left_motor_duty
-	wheel_track=0.106
-	right_motor_duty=(msg.linear.x+wheel_track/2*msg.angular.z)*100
-	left_motor_duty=(msg.linear.x-wheel_track/2*msg.angular.z)*100
+	wheel_track=1
+	right_motor_duty=(msg.linear.x+wheel_track*msg.angular.z)*100
+	left_motor_duty=(msg.linear.x-wheel_track*msg.angular.z)*100
+	print("right_motor_duty",right_motor_duty)
+	print("left_motor_duty",left_motor_duty)
+	right_motor_duty=limit_duty(right_motor_duty)
+	left_motor_duty=limit_duty(left_motor_duty)
 	print("right_motor_duty",right_motor_duty)
 	print("left_motor_duty",left_motor_duty)
 
+
+print("Subscribing the /cmd_vel topic...")
 rospy.init_node('inv_kinematics_node')
 sub = rospy.Subscriber('/cmd_vel', Twist, callback)
 rospy.spin()
