41b05687f0af2c1a481011087ada08294e6ba726
saricay <33890430+saricay@users.noreply.github.com>
2021-06-13 01:11:18 +0300

    Add files via upload

--- src/inverse_kinematics.py
+++ src/inverse_kinematics.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
-from gpio_configuration import *
-
 def limit_duty(duty):
+	print duty
 	if abs(duty)>100:
+		print("Duty is regulated")
 		duty=duty/abs(duty)*100
 	else:
+		print("Duty is OK")
 		duty=duty
 	return duty
 
@@ -14,23 +15,13 @@ def callback(msg):
 	global right_motor_duty, left_motor_duty
 	wheel_track=1
 	right_motor_duty=(msg.linear.x+wheel_track*msg.angular.z)*100
-	left_motor_duty=(msg.linear.x-wheel_track*msg.angular.z)*140
+	left_motor_duty=(msg.linear.x-wheel_track*msg.angular.z)*100
+	print("right_motor_duty",right_motor_duty)
+	print("left_motor_duty",left_motor_duty)
 	right_motor_duty=limit_duty(right_motor_duty)
 	left_motor_duty=limit_duty(left_motor_duty)
-	print("right_motor_duty:",right_motor_duty)
-	print("left_motor_duty:",left_motor_duty)
-	if(right_motor_duty>0 and left_motor_duty>0):
-		forward()
-	elif(right_motor_duty>0 and left_motor_duty<0):
-		turn_left()
-	elif(right_motor_duty<0 and left_motor_duty>0):
-		turn_right()
-	elif(right_motor_duty<0 and left_motor_duty<0):
-		backward()
-	elif(right_motor_duty==0 and left_motor_duty==0):
-		stop()
-	p1.start(abs(right_motor_duty))
-	p2.start(abs(left_motor_duty))
+	print("right_motor_duty",right_motor_duty)
+	print("left_motor_duty",left_motor_duty)
 
 
 print("Subscribing the /cmd_vel topic...")
